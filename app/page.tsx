38980ac0adb0bfbe82caa049c0aac8dad580ae3d
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2025-04-07 11:54:52 +0000

    This is the the final records

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 import Services from "./components/Services";
+// This is the main page where all the imports are done.
+
 
 export default function Home() {
   return (
